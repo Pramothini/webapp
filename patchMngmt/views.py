c9557c566cb75a00e1e2d7bbcba3a08dc73c52f8
Pramothini <pramothinidk@gmail.com>
2015-09-18 17:52:28 -0700

    Test run
    Added tables.html

--- patchMngmt/views.py
+++ patchMngmt/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def home(request):
     # Sets up list of just the logged-in user's (request.user's) items
     return render(request, 'index.html')
+
+
+def getTables(request):
+    # Sets up list of just the logged-in user's (request.user's) items
+    return render(request, 'tables.html')
--- webapp/urls.py
+++ webapp/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'patchMngmt.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^tables$', 'patchMngmt.views.getTables', name='getTables'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
