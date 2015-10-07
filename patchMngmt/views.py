18db466f6166cba701665f998aa1d06014e7c575
Pramothini <pramothinidk@gmail.com>
2015-10-07 00:05:21 -0700

    able to populate table from csv file

--- patchMngmt/views.py
+++ patchMngmt/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+import csv
+import os
 
 # Create your views here.
 def home(request):
-    # Sets up list of just the logged-in user's (request.user's) items
-    return render(request, 'index.html')
+    workpath = os.path.dirname(os.path.abspath(__file__)) #Returns the Path your .py file is in
+    resultlist = []
+    with open(workpath+'/static/webapp_input.csv', 'rt') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            resultlist.append(row)
+    return render(request, 'index.html',{'resultlist':resultlist})
 
 
 def getTables(request):
