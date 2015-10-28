bce63241bcfb44cd787e851ecc0c59b8ee9c1a8a
Pramothini <pramothinidk@gmail.com>
2015-10-27 19:25:38 -0700

    displays data from a static cvs file

--- showReport/views.py
+++ showReport/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import AssetRating
+import csv
+import os
 # Create your views here.
 def mainMenu(request):
     context=AssetRating.objects.all()
@@ -10,10 +12,10 @@ def getAssets(request):
     return render(request, 'assetData.html', {"context":context.values()})
 
 def home(request):
-    # workpath = os.path.dirname(os.path.abspath(__file__)) #Returns the Path your .py file is in
-    # resultlist = []
-    # with open(workpath+'/static/webapp_input.csv', 'rt') as f:
-    #     reader = csv.DictReader(f)
-    #     for row in reader:
-    #         resultlist.append(row)
-    return render(request, 'home.html')
+    workpath = os.path.dirname(os.path.abspath(__file__)) #Returns the Path your .py file is in
+    resultlist = []
+    with open(workpath+'/static/webapp_input.csv', 'rt') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            resultlist.append(row)
+    return render(request, 'home.html',{'resultlist':resultlist})
