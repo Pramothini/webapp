9054763eb0ac49cd4d6e860e805033bc388fc655
Pramothini <pramothinidk@gmail.com>
2015-10-27 23:28:59 -0700

    stored data in table in db

--- showReport/views.py
+++ showReport/views.py
@@ -12,10 +12,12 @@ def getAssets(request):
     return render(request, 'assetData.html', {"context":context.values()})
 
 def home(request):
-    workpath = os.path.dirname(os.path.abspath(__file__)) #Returns the Path your .py file is in
-    resultlist = []
-    with open(workpath+'/static/webapp_input.csv', 'rt') as f:
-        reader = csv.DictReader(f)
-        for row in reader:
-            resultlist.append(row)
-    return render(request, 'home.html',{'resultlist':resultlist})
+    # workpath = os.path.dirname(os.path.abspath(__file__)) #Returns the Path your .py file is in
+    # resultlist = []
+    # with open(workpath+'/static/webapp_input.csv', 'rt') as f:
+    #     reader = csv.DictReader(f)
+    #     for row in reader:
+    #         resultlist.append(row)
+    context=AssetRating.objects.all()
+    print context.values()
+    return render(request, 'home.html',{"context":context.values()})
